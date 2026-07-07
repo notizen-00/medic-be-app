eaabacc47f0dae918815f6c297d5bc2cb5d83f1b
Notizen__ <dipta.ikromi@unej.ac.id>
2026-07-07 11:34:21 +0700

    fixed reverb production

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,14 @@ Route::get('/', function () {
 });
 
 Route::get('/mitra/login', function () {
+    $appUrl = config('app.url');
+    $defaultScheme = parse_url($appUrl, PHP_URL_SCHEME) ?: (request()->isSecure() ? 'https' : 'http');
+    $defaultHost = parse_url($appUrl, PHP_URL_HOST) ?: request()->getHost();
+
     return view('mitra.login', [
         'reverbKey' => env('REVERB_APP_KEY', 'medic-app-key'),
-        'reverbHost' => env('VITE_REVERB_HOST', request()->getHost()),
-        'reverbPort' => env('VITE_REVERB_PORT', '8080'),
-        'reverbScheme' => env('VITE_REVERB_SCHEME', request()->isSecure() ? 'https' : 'http'),
+        'reverbHost' => env('VITE_REVERB_HOST', $defaultHost),
+        'reverbPort' => env('VITE_REVERB_PORT', $defaultScheme === 'https' ? '443' : '8080'),
+        'reverbScheme' => env('VITE_REVERB_SCHEME', $defaultScheme),
     ]);
 });
